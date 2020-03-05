8a3dd1565dfff9f29babe855a510da5483ad4745
Davide <davide.roberti89@gmail.com>
2020-03-05 14:59:06 +0100

    messaggio di risposta aggiunto

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('.invio').click(function(){
         invioMessaggio();
+
+        setTimeout(messaggioRicevuto, 1000); 
     });
 
     function invioMessaggio () {
@@ -12,11 +14,26 @@ $(document).ready(function(){
         $('.container-messaggi').append(messaggioTipo);
     };
 
+    function messaggioRicevuto () {
+        var messageText = 'Vabbé'
+        var messaggioTipo = $('.template-messaggio .message').clone().addClass('received');
+        messaggioTipo.find('.true-message').text(messageText);
+        messaggioTipo.find('.orario-messaggio').text(orarioAttuale);
+        $('.container-messaggi').append(messaggioTipo);
+    };
+
     function orarioAttuale () {
         var data = new Date();
-        var oraNow = data.getHours();
-        var minutiNow = data.getMinutes();
+        var oraNow = addZero(data.getHours());
+        var minutiNow = addZero(data.getMinutes());
         var tempoNow = oraNow + '.' + minutiNow;
         return tempoNow;
+    }
+
+    function addZero(i) {
+        if (i < 10) {
+        i = "0" + i;
+        }
+        return i;
     }
 });
