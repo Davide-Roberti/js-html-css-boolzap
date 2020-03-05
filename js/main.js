6f781efabfb3d53700bf7bd58c886352bc7acced
Davide <davide.roberti89@gmail.com>
2020-03-05 11:51:54 +0100

    ora dei messaggi in tempo reale

--- js/main.js
+++ js/main.js
@@ -4,9 +4,15 @@ $(document).ready(function(){
         $('#testo-messaggio').val('');
         var messaggioTipo = $('.template-messaggio .message').clone().addClass('sent');
         messaggioTipo.find('.true-message').text(messageText);
-        messaggioTipo.find('.orario-messaggio').text('11.32');
+        messaggioTipo.find('.orario-messaggio').text(orarioAttuale);
         $('.container-messaggi').append(messaggioTipo);
     });
 
-    
+    function orarioAttuale () {
+        var data = new Date();
+        var oraNow = data.getHours();
+        var minutiNow = data.getMinutes();
+        var tempoNow = oraNow + '.' + minutiNow;
+        return tempoNow;
+    }
 });
